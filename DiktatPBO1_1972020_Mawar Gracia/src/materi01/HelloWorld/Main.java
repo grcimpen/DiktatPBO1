1a165906aff0752a567f4c5ab1bb49ff63d4ac34
grcimpen <56014925+grcimpen@users.noreply.github.com>
2021-06-14 20:22:40 +0700

    Add files via upload

---
+++ DiktatPBO1_1972020_Mawar Gracia/src/materi01/HelloWorld/Main.java
@@ -0,0 +1,11 @@
+package materi01.HelloWorld;
+
+/*
+    @author Mawar Gracia (1972020)
+*/
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
